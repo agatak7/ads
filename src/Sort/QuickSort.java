eb43eed9396cf8ed11d5b2e292fee446b6884b7f
agata <a.n.kordyl@student.tudelft.nl>
2019-12-14 20:24:40 +0100

    in place quick sort!

---
+++ src/Sort/QuickSort.java
@@ -0,0 +1,51 @@
+package Sort;
+
+public class QuickSort {
+
+    public static void quickSort(int[] elements) {
+        inPlaceQuickSort(elements, 0, elements.length - 1);
+    }
+
+    //sorts a sub-array [a...b]
+    private static void inPlaceQuickSort(int[] arr, int a, int b) {
+        //base case
+        if(a>=b) return;
+
+        //select pivot as last element
+        int pivot = arr[b];
+
+        //start at both ends
+        int left = a;
+        int right = b - 1;
+
+        //for swap
+        int temp;
+
+        while(left<=right) {
+
+            //traverse from left until elegible element or right marker
+            while (left <= right && arr[left] < pivot) left++;
+
+            //same from right
+            while (left <= right && arr[right] > pivot) right--;
+
+            //if left/right not crossed, swap
+            if (left <= right) {
+                temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+
+        //at the end place pivot at left index
+        temp = arr[left];
+        arr[left] = arr[b];
+        arr[b] = temp;
+
+        //continue recursion
+        inPlaceQuickSort(arr, a, left - 1);
+        inPlaceQuickSort(arr, left + 1, b);
+    }
+}
---
+++ src/Sort/sortTests.java
@@ -0,0 +1,26 @@
+package Sort;
+
+import Sort.Sort1;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class sortTests {
+
+    @Test
+    public void testSort1() {
+        int[] data = {1, 2, 3, 4, 5, 6, 8};
+        int[] res = {8, 6, 5, 4, 3, 2, 1};
+        Sort1.foo(data.length , data);
+        assertArrayEquals(data, res);
+    }
+
+    @Test
+    public void quickTest() {
+        int[] data = {11, 90, 33, 71, 24, 50, 35, 30, 15, 21};
+        int[] data2 = {11, 15, 21, 24, 30, 33, 35, 50, 71, 90};
+        QuickSort.quickSort(data);
+        assertArrayEquals(data2, data);
+    }
+
+}
